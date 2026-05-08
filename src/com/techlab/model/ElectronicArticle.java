ad6e1ad20152b81e7396ee768640c1309749ebc2
emilianoagustin <pintos.emiliano@gmail.com>
2026-05-08 11:14:13 -0300

    Created child article classes

---
+++ src/com/techlab/model/ElectronicArticle.java
@@ -0,0 +1,30 @@
+package model;
+
+public class ElectronicArticle extends Article {
+  private int warrantyMonths;
+
+  public ElectronicArticle(int code, String name, double price, Category category, int warrantyMonths){
+    super(code, name, price, category);
+    this.warrantyMonths = warrantyMonths;
+  }
+
+  public int getWarrantyMonths() {
+    return this.warrantyMonths;
+  }
+  public void setWarrantyMonths(int months) {
+    this.warrantyMonths = months;
+  }
+  
+  @Override
+  public String getArticleType() {
+    return " ";
+  }
+  @Override
+  public double calculateFinalPrice() {
+    return 0;
+  }
+  @Override
+  public String getSpecificDetail() {
+    return " ";
+  }
+}
