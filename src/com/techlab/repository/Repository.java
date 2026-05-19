44924bcfbb1095d9b7f289549cdb42914e53509e
emilianoagustin <pintos.emiliano@gmail.com>
2026-05-18 22:18:33 -0300

    added Repository

--- src/com/techlab/repository/Repository.java
+++ src/com/techlab/repository/Repository.java
@@ -2,30 +2,44 @@ package repository;
 
 import interfaces.Identifiable;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Repository<T extends Identifiable> {
   private ArrayList<T> list = new ArrayList<>();
 
   public boolean addToList(T object) {
-    return true;
+    if(object == null) {
+      return false;
+    }
+    if(searchByCode(object.getCode()) != null) {
+      return false;
+    }
+    return list.add(object);
   }
   
-  public ArrayList<T> getList() {
-    return list;
+  public List<T> getList() {
+    return new ArrayList<>(list);
   }
   
   public T searchByCode(int code) {
-    T object;
-    return object;
+    for(T object : list) {
+      if(object.getCode() == code) {
+        return object;
+      }
+    };
+    return null;
   }
 
   public boolean delete(T object) {
-    return true;
+    if(object == null) {
+      return false;
+    }
+    return list.remove(object);
   }
   
   public boolean checkEmpty() {
     if(list.isEmpty()) {
-      System.out.println("There's no items in the catalogue.");
+      System.out.println("There's no items in this list.");
       return true;
     }
     return false;
